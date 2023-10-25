53c01ce8bcb7e1e693d136370cda3071e16082b9
parthct45 <parthmane74112@gmail.com>
2023-10-25 17:02:43 +0530

    Added blur Image functionality

--- Image Manipulation/filters.c
+++ Image Manipulation/filters.c
@@ -1,5 +1,6 @@
 #include "filters.h"
 
+// Grayscale 
 void grayScale(qtNode *t1, qtNode **res)
 {
     (*res) = malloc(sizeof(qtNode));
@@ -29,6 +30,7 @@ void grayScale(qtNode *t1, qtNode **res)
     return;
 }
 
+// Negative image 
 void negativeImage(qtNode *t1, qtNode **res)
 {
     (*res) = malloc(sizeof(qtNode));
@@ -57,6 +59,7 @@ void negativeImage(qtNode *t1, qtNode **res)
     return;
 }
 
+// Sepia filter 
 void sepia(qtNode* t1,qtNode** res)
 {
     (*res) = malloc(sizeof(qtNode));
@@ -85,4 +88,58 @@ void sepia(qtNode* t1,qtNode** res)
     (*res)->area = t1->area;
     return;
 
+}
+
+// Blur image 
+
+void blurImage(qtNode *t1, qtNode **res)
+{
+    (*res) = malloc(sizeof(qtNode));
+
+    if (t1->topLeft != NULL)
+    {
+        blurImage(t1->topLeft, &(*res)->topLeft);
+        blurImage(t1->topRight, &(*res)->topRight);
+        blurImage(t1->bottomLeft, &(*res)->bottomLeft);
+        blurImage(t1->bottomRight, &(*res)->bottomRight);
+    }
+    else
+    {
+        (*res)->topRight = NULL;
+        (*res)->topLeft = NULL;
+        (*res)->bottomLeft = NULL;
+        (*res)->bottomRight = NULL;
+    }
+
+    // Calculate the average RGB values for the current node and its neighbors
+    int totalRed = t1->p.red;
+    int totalGreen = t1->p.green;
+    int totalBlue = t1->p.blue;
+
+    if (t1->topLeft != NULL)
+    {
+        totalRed += t1->topLeft->p.red;
+        totalGreen += t1->topLeft->p.green;
+        totalBlue += t1->topLeft->p.blue;
+
+        totalRed += t1->topRight->p.red;
+        totalGreen += t1->topRight->p.green;
+        totalBlue += t1->topRight->p.blue;
+
+        totalRed += t1->bottomLeft->p.red;
+        totalGreen += t1->bottomLeft->p.green;
+        totalBlue += t1->bottomLeft->p.blue;
+
+        totalRed += t1->bottomRight->p.red;
+        totalGreen += t1->bottomRight->p.green;
+        totalBlue += t1->bottomRight->p.blue;
+    }
+
+    (*res)->p.red = totalRed / 5;     // Current node + 4 neighbors
+    (*res)->p.green = totalGreen / 5;
+    (*res)->p.blue = totalBlue / 5;
+
+    (*res)->area = t1->area;
+
+    return;
 }
--- Image Manipulation/filters.h
+++ Image Manipulation/filters.h
@@ -3,3 +3,4 @@
 void grayScale(qtNode* t1,qtNode** res);
 void negativeImage(qtNode* t,qtNode** res);
 void sepia(qtNode* t1,qtNode** res);
+void blurImage(qtNode* t1, qtNode** res) ; 
