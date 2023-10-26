e5944be796861a57dd452561af579581587a423a
parthct45 <parthmane74112@gmail.com>
2023-10-26 14:43:00 +0530

    Changes made

--- Image Manipulation/filters.h
+++
@@ -1,6 +0,0 @@
-#include "union.h"
-
-void grayScale(qtNode* t1,qtNode** res);
-void negativeImage(qtNode* t,qtNode** res);
-void sepia(qtNode* t1,qtNode** res);
-void blurImage(qtNode* t1, qtNode** res) ; 
